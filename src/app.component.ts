42f3e9a91f0bb1264b81971973a4ebf8b8e054e9
nikhiltrash <git@stackblitz.com>
2024-11-17 14:20:59 +0530

    Initial commit

---
+++ src/app.component.ts
@@ -0,0 +1,52 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ComboBoxModule } from '@syncfusion/ej2-angular-dropdowns';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [FormsModule, ComboBoxModule],
+})
+export class App implements OnInit, AfterViewInit {
+  public physicalVerificationForm: any = {};
+
+  public batchInfo: any;
+  public panelistsFields: object = { text: 'name' };
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+  ngAfterViewInit(): void {}
+
+  loadData() {
+    const apiResponse = {
+      panelistList1: {
+        name: 'ABC',
+        emailId: 'abc@yopmail.com',
+      },
+      panelistList2: {
+        name: 'XYZ',
+        emailId: 'xyz@yopmail.com',
+      },
+    };
+    this.physicalVerificationForm = apiResponse;
+    console.log(this.physicalVerificationForm);
+    this.batchInfo = {
+      panelists: [
+        { name: 'ABC', emailId: 'abc@yopmail.com' },
+        { name: 'XYZ', emailId: 'xyz@yopmail.com' },
+      ],
+    };
+  }
+
+  onPanelistSelect(event: any, panelist: string) {
+    console.log(event);
+    console.log(this.physicalVerificationForm);
+    this.physicalVerificationForm[panelist] = event.itemData;
+  }
+
+  onSubmit() {
+    console.log(this.physicalVerificationForm);
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+
+import { App } from './app.component';
+
+bootstrapApplication(App);
